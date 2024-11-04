a0ab833b7508c850f67265c7600fb77b0c09ac3d
keitheorem <152051386+keitheorem@users.noreply.github.com>
2024-11-04 16:10:54 +0800

    Update inverse_kinematics.py

--- arm_control/inverse_kinematics.py
+++ arm_control/inverse_kinematics.py
@@ -2,13 +2,11 @@ import ikpy.chain
 import numpy as np
 import ikpy.utils.plot as plot_utils
 import matplotlib.pyplot as plt
-# # from ros_robot_controller.ros_robot_controller_sdk import Board
+from ros_robot_controller.ros_robot_controller_sdk import Board
 
-# board = Board(device="/dev/rrc", baudrate=1000000, timeout=5)
-# board.enable_reception()
+board = Board(device="/dev/rrc", baudrate=1000000, timeout=5)
+board.enable_reception()
 # print(board.pwm_servo_read_position(1))
-
-# board.bus_servo_set_position(5, [(1,500)])
 
 my_chain = ikpy.chain.Chain.from_urdf_file("/home/keitheorem/torch/robot_arm.urdf", active_links_mask = [False, True, True, True, True, True, False])
 max_angle = 2.0944 #i.e. 120 degrees 
@@ -28,12 +26,12 @@ servo = [1,2,3,4,5]
 for i in range(0,5):
     servo_command[i] = int(500 - ((servo_angle[servo[i]]*500)/max_angle))
 
-print(servo_command)
-# board.bus_servo_set_position(5, [[1,servo_angle[1], [2,servo_angle[2]], [3,servo_angle[3]], [4,servo_angle[4]], [5,servo_angle[5]]])
+board.bus_servo_set_position(5, [[1,servo_angle[1], [2,servo_angle[2]], [3,servo_angle[3]], [4,servo_angle[4]], [5,servo_angle[5]]])
 
-fig, ax = plot_utils.init_3d_figure()
-my_chain.plot(servo_angle, ax, target=target_position)
-plt.xlim(-0.5, 0.5)
-plt.ylim(-0.5, 0.5)
-plt.show()
+# To visualise virtually
+# fig, ax = plot_utils.init_3d_figure()
+# my_chain.plot(servo_angle, ax, target=target_position)
+# plt.xlim(-0.5, 0.5)
+# plt.ylim(-0.5, 0.5)
+# plt.show()
 
